c9945cd5f87552fbb6217a318bb682a6e66d095e
OtmanEss <46670904+OtmanEss@users.noreply.github.com>
2019-06-14 15:22:48 +0200

    Image upload save and views

--- Intec_WebShop/IntecWebShopp.WebUI/Controllers/ProductManagerController.cs
+++ Intec_WebShop/IntecWebShopp.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,17 +16,17 @@ namespace IntecWebShopp.WebUI.Controllers
         IRepository<Product> context;
         IRepository<ProductCategory> productCategoryContext;
 
-       
 
-        public ProductManagerController(IRepository<Product> productContext, IRepository<ProductCategory> categoryContext )
+
+        public ProductManagerController(IRepository<Product> productContext, IRepository<ProductCategory> categoryContext)
         {
-            context =productContext;                              
-            productCategoryContext =categoryContext;
+            context = productContext;
+            productCategoryContext = categoryContext;
 
             //productCategoryContext = new InMemoryRepository<ProductCategory>();       // acces a toutes les fonctionnalites de productcategory
             //context = new InMemoryRepository<Product>();                              // acces a toutes les fct de product
 
-        }      
+        }
 
         public ActionResult Index()
         {
@@ -47,16 +48,23 @@ namespace IntecWebShopp.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (ModelState.IsValid == false)        // check si le model est completé correctement 
             {
                 return View(product);
             }
-            context.Insert(product);
-            context.Commit();
+            else
+            {
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages/") + product.Image);
+                }
+                context.Insert(product);
+                context.Commit();
+            }
             return RedirectToAction("Index");
-
         }
 
         [HttpGet]
@@ -64,9 +72,9 @@ namespace IntecWebShopp.WebUI.Controllers
         {
             // trouver le produit 
             var productToDelete = context.FindById(id);
-           
-                // affiche le product to delete
-                return View(productToDelete);           
+
+            // affiche le product to delete
+            return View(productToDelete);
         }
 
         [HttpPost]
@@ -80,18 +88,18 @@ namespace IntecWebShopp.WebUI.Controllers
                 return HttpNotFound();
             }
             else
-            {            
+            {
                 return RedirectToAction("Index");
             }
         }
 
         [HttpGet]
         public ActionResult Edit(string id)
-        {           
+        {
             // find the product
             var productToUpdate = context.FindById(id);
 
-            if (productToUpdate==null)
+            if (productToUpdate == null)
             {
                 return HttpNotFound();
             }
@@ -99,20 +107,27 @@ namespace IntecWebShopp.WebUI.Controllers
             ProductManagerViewModel viewModel = new ProductManagerViewModel();
             viewModel.Product = productToUpdate;
             viewModel.productCategories = productCategoryContext.Collection();
-
             return View(viewModel);
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product)
+        public ActionResult Edit(Product product, HttpPostedFileBase file)
         {
-            context.Update(product);
-            //context.Delete(product.Id);     //delete old product
-            //context.Insert(product);        // insert new
-            context.Commit();
-            return RedirectToAction("Index");            
+            if (ModelState.IsValid)
+            {
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages/") + product.Image);
+                }
+                    context.Update(product);
+                    //context.Delete(product.Id);     //delete old product
+                    //context.Insert(product);        // insert new
+                    context.Commit();
+            }
+            return RedirectToAction("Index");
         }
-       
-       
+
+
     }
 }
