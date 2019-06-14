f779d1802efed8c39efdd47bf861076334ce0f1e
OtmanEss <46670904+OtmanEss@users.noreply.github.com>
2019-06-14 10:08:59 +0200

    create db sql

---
+++ Intec_WebShop/IntecWebShop.DataAccess.SQL/Contexts/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using IntecWebShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntecWebShop.DataAccess.SQL.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext():base("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> Categories { get; set; }
+    }
+}
