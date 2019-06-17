1ce91f7ddc27807ab59c72f0f14c2992cdc0bf59
OtmanEss <46670904+OtmanEss@users.noreply.github.com>
2019-06-17 10:51:08 +0200

    home page list product

--- Intec_WebShop/IntecWebShopp.WebUI/Controllers/HomeController.cs
+++ Intec_WebShop/IntecWebShopp.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IntecWebShop.Core.Interfaces;
+using IntecWebShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,19 @@ namespace IntecWebShopp.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategoryContext;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> categoryContext)
+        {
+            context = productContext;
+            productCategoryContext = categoryContext;      
+        }
+        
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
         }
 
         public ActionResult About()
